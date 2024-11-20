55e8ba35c16b54bb41abbfd31f03c0f683f694c8
habtomgebre <hatomg127@gmail.com>
2024-11-19 22:22:14 -0600

    create a project

---
+++ src/test/java/com/config_repo_demo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config_repo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
